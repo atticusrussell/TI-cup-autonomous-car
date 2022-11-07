5f8beca5100aed3a5997f5e1b8775e835415f309
atticusrussell <atticusrussell@gmail.com>
2022-11-07 01:52:35 -0500

    broke up into functions

--- src/ServoMotor.c
+++ src/ServoMotor.c
@@ -2,7 +2,7 @@
  * @file ServoMotor.c
  * @authors Erica Coles, Atticus Russell
  * @brief file to interface with servo steering IDE msp432 car project
- * @note trying to make it modular and re-usable
+ * @note trying to make it modular and re-usable. Customized for MG996r servo.
  * @version 0.1
  * @date 11-04-2022
  * 
@@ -10,23 +10,28 @@
  */
 
 
-#include "msp.h"
 #include <stdint.h>
 #include <stdio.h>
+#include "msp.h"
 #include "TimerA.h"
+#include "ServoMotor.h"
 
-// adjustable parameters
-uint16_t timerPeriod;
-uint16_t delayMs; // delay constant
-uint16_t servoPeriod; //20 cycles	
 
+
+// which way to turn but encoded as ints
+enum turnDir{
+	Left = -1,
+	Right = 1
+} direction;
+
+
+uint16_t servoPeriod;
+double oneDegDC = 0.5/MAX_ANGLE_DEG;
 
 
 
 // TODO create a header file
 
-
-// TODO create adjust func and initialization func instead of main 
 	
 void servo_init(void){
 	/* start puzzling comments from prior lab */
@@ -49,20 +54,58 @@ void servo_init(void){
 		* 	A 1.0ms pulse says to turn 90o	counter-clockwise
 		* 	A 2.0ms pulse says to turn 90o in the clockwise direction
 		*/
-	timerPeriod = 60000;
-	servoPeriod = 20; //20 cycles
+	servoPeriod = SERVO_CLK/PWM_FREQ_HZ;
+	double servoStraightDC = 1.5/servoPeriod;
 	
-	// TODO start with servo straight -> initialize to 1.5 Ms
-	// start at 1.0 ms pulse - should be 90 ccw
-	TIMER_A2_PWM_Init(timerPeriod, 1.5/servoPeriod, 1); 
+	// TODO start with servo straight -> initialize to 1.5 ms
+	TIMER_A2_PWM_Init(servoPeriod, servoStraightDC, 1); 
 }
 
 
-
-void set_servo()
-	{
-
-		//pulse of 1.x ms should xx the servo motor
-		TIMER_A2_PWM_DutyCycle(2.0 / servoPeriod, 1);
-		
+/**
+ * @brief Set the duty cycle of the servo
+ * 
+ * @param servoDC the new duty cycle to set the servo to. is a num 0 to 1 
+ */
+void set_servo_DC(double servoDC){
+	TIMER_A2_PWM_DutyCycle(servoDC, 1);
 }
+
+
+/**
+ * @brief takes the sign of an int
+ * 
+ * @param x 
+ * @return int: -1 if negative, 1 if positive
+ */
+int sign(int x) {
+    return (x > 0) - (x < 0);
+}
+
+
+/**
+ * @brief Set the angle of the steering in degrees
+ * 
+ * @param steeringAngle an angle in degrees to set the steering. -left, +right
+ * @note using int for now - FUTURE convert to float if more precision needed
+ */
+void set_steering_deg(int16_t steeringAngle){
+	direction = sign(steeringAngle); 
+	// make sure that it is within the correct bounds
+	if (abs(steeringAngle) > MAX_ANGLE_DEG){
+		if(direction == Left){
+			steeringAngle = -MAX_ANGLE_DEG;
+		} else{
+			steeringAngle = MAX_ANGLE_DEG;
+		}
+	}
+	/* A 1.5ms pulse says do not turn at all
+	* 	A 1.0ms pulse says to turn 90o	counter-clockwise
+	* 	A 2.0ms pulse says to turn 90o in the clockwise direction	 */
+	// FUTURE maybe don't do a floating point multiply in the future
+	double servoDC= 1 +  (direction * steeringAngle * oneDegDC);
+	
+	set_servo_DC(servoDC);
+
+}
+	
