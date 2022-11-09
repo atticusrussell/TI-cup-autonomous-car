ce3b3f165d36393b3e23085f9f46a1507ebbdc81
atticusrussell <atticusrussell@gmail.com>
2022-11-08 21:49:58 -0500

    moved some stuff around. This is an unhelpful commit message. deal with it future self.

--- src/CarMain.c
+++ src/CarMain.c
@@ -12,6 +12,7 @@
 #include "Common.h"
 #include "ServoMotor.h"
 #include "DCMotors.h"
+#include "CortexM.h"
 
 #define 	BASIC_TESTING
 
@@ -20,9 +21,15 @@
 
 
 // FUTURE implement error handling
-
-
 /* define the variables here that define state of car and we're gonna modify */
+
+
+/* camera variables*/
+uint16_t line[128];			// current array of raw camera data
+uint16_t filterLine[128];	// camera data filtered by smoothing
+BOOLEAN	g_sendData;			// if the camera is ready to send new data
+
+
 // number from 0 to 100 that will be converted into duty cycle for motor
 int motorSpeed; 
 
@@ -62,16 +69,18 @@ onTrack = 1;
 // [x] test with hardware the servo function
 // [x] create abstract DC motor speed function in this file
 // [x] implement motor speed and dir funcs in existing file
-// [ ] make DC motor header file
-// [ ] test in hardware the motor function
-// [ ] test camera functionality/ look at the MATLAB plot of the track lab5 code
+// [x] make DC motor header file
+// [x] test in hardware the motor function
+// [x] test camera functionality/ look at the MATLAB plot of the track lab5 code
 // [ ] create function to use the camera to see if on the track
 // [ ] create specific carpet detection function in some file (milestone 1)
 // [ ] create function to detect edge with camera
 // [ ] create function to report edge direction with camera 
-// NOTE maybe direction not totally necessary - could possibly pick a direction at random and just see if edge gets closer or further.
+// [ ] figure out what direction the center of the track is
+// [ ] turn the wheels towards the center of the track
 
-
+// FUTURE use switches and OLED to selelct different track mode and display
+// FUTURE use interrupts when doing track edge detection for optimization
 
 
 
@@ -91,11 +100,17 @@ void delay(int del){
 
 // NOTE we want to abstract away the hardware details in main
 int main(void){
+	DisableInterrupts();
 	// call initialization function for servo
 	servo_init();
 	// TODO call initialization function for motor
+	DC_motors_init();
 
-	// TODO call initialization function for camera
+	// TODO write and call initialization function for camera
+
+	// enable interrupts so the camera updates
+	EnableInterrupts();
+
 
 	// infinite loop to contain logic
 	while(1){
