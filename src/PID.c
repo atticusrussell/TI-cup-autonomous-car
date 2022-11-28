ac5e757070a23cd1341654539ec4bf8e8876e94f
atticusrussell <atticusrussell@gmail.com>
2022-11-27 19:09:08 -0500

    created PID modules

---
+++ src/PID.c
@@ -0,0 +1,16 @@
+/**
+ * @file PID.c
+ * @author Atticus Russell 
+ * @brief PID control for IDE MSP432 car
+ * @version 0.1
+ * @date November 2022
+ * @class CMPE-460 Interface and Digital Electronics
+ * 
+ * 
+ */
+
+#include	<stdint.h>
+#include "Common.h"
+#include "PID.h"
+
+
---
+++ src/PID.h
@@ -0,0 +1,27 @@
+/**
+ * @file PID.h
+ * @author Atticus Russell
+ * @brief header file for IDE MSP432 car PID control
+ * @version 0.1
+ * 
+ * 
+ */
+ 
+#ifndef _PID_HEADER_FILE_
+#define _PID_HEADER_FILE_
+
+typedef struct pid_nums{
+	float kp;
+	float ki;
+	float kd;
+} pid_nums_t;
+
+
+ /* Function prototypes */ 
+
+// void INIT_Camera(void);
+// BOOLEAN get_on_track(uint16_t maxCamVal);
+// uint8_t get_track_center(uint16_t* smoothData);
+// void smooth_line(uint16_t* rawData, uint16_t* smoothData);
+
+ #endif
